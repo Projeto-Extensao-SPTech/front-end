05f61eb315159a17d6699213c70d44deede85914
luanaliriel <luana.bsantos@sptech.school>
2025-10-31 16:18:02 -0300

    subindo ajustes na seção da home

--- src/components/sections/BemVindos.jsx
+++ src/components/sections/BemVindos.jsx
@@ -1,7 +1,7 @@
 import Button from '../ui/Button'
 import { Link } from 'react-scroll'
 
-export default function Sobre() {
+export default function BemVindos() {
     return (
         <div className="min-h-[50vh] flex items-center justify-center bg-[#EFEFEF] py-8 relative z-10">
 
@@ -21,7 +21,7 @@ export default function Sobre() {
                             Transformando vidas de animais abandonados através do amor e cuidado
                         </p>
 
-                        <div className="mt-1">
+                        <div className="mt-4 lg:mt-1"> {/* Aumentei o margin-top no mobile */}
                             <Link to="voluntariado" spy={true} smooth={true} duration={500}>
                                 <Button
                                     className="shadow-[inset_0_2px_4px_0_rgba(0,0,0,0.2)] bg-[#FCAD0B] hover:bg-[#052759] hover:[#052759] text-sm mx-auto"
@@ -32,11 +32,11 @@ export default function Sobre() {
                         </div>
                     </div>
 
-                    <div className="flex justify-center lg:justify-end w-full lg:w-1/2 -mt-4 lg:mt-0">
+                    <div className="flex justify-center lg:justify-end w-full lg:w-1/2 mt-4 lg:-mt-6"> {/* Removi -mt-4 do mobile e adicionei mt-4 */}
                         <img
                             src="/img-pets1-sobre.png"
                             alt="Cachorros felizes do abrigo"
-                            className="w-72 md:w-80 lg:w-96 h-auto rounded-lg transform hover:scale-105 transition-transform duration-300 -mt-6"
+                            className="w-60 sm:w-72 md:w-80 lg:w-96 h-auto rounded-lg transform hover:scale-105 transition-transform duration-300"
                         />
                     </div>
                 </div>
@@ -49,7 +49,7 @@ export default function Sobre() {
                             <img
                                 src="/imgs-sobre.png"
                                 alt="Ajude nossos amigos"
-                                className="w-32 md:w-36 lg:w-60 h-auto rounded-lg transform hover:scale-105 transition-transform duration-300 mb-2"
+                                className="w-28 sm:w-32 md:w-36 lg:w-60 h-auto rounded-lg transform hover:scale-105 transition-transform duration-300 mb-2"
                             />
 
                             <p className="text-[#052759] text-lg font-bold mb-1">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import Sobre from '../components/sections/Sobre'
 import Participar from '../components/sections/Participar'
 import Adotados from '../components/sections/Adotados'
 import FaleConosco from '../components/sections/FaleConosco'
+import BemVindos from '../components/sections/BemVindos'
 
 export default function Home() {
     return (
         <div>
-            <Sobre />
+            <BemVindos />
             <Participar />
             <Adotados />
             <FaleConosco />
