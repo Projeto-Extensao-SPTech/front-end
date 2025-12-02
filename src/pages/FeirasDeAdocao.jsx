0d77f8abc270ce69d4984864f3f37cc6de99b449
luanaliriel <luana.bsantos@sptech.school>
2025-12-02 18:00:41 -0300

    feat(fair-addoption): add pet images and update background visuals

--- src/pages/FeirasDeAdocao.jsx
+++ src/pages/FeirasDeAdocao.jsx
@@ -51,12 +51,13 @@ export default function FeirasDeAdocao() {
 
     function randomImage() {
         const images = [
+            '/img-card1.png',
+            '/img-card2.png',
+            '/img-card3.png',
             '/img-card4.png',
             '/img-card5.png',
             '/img-card6.png',
-            '/img-card8.png',
-            '/img-card9.png',
-            '/img-card10.png'
+            '/img-card7.png'
         ];
         return images[Math.floor(Math.random() * images.length)];
     }
